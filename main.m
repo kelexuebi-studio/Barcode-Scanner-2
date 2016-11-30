bf84bd7583a0bafbf0a6235c29203a1ab52382f3
Sandro Magalhães <sandro.magalhaes@fe.up.pt>
2016-11-30 00:39:04 +0000

    Barcode Decode

--- main.m
+++ main.m
@@ -2,21 +2,92 @@ clc
 clear
 close all
 
-img_or=imread('ean-13.png');
-img_gray=rgb2gray(img_or);
-img_b=im2bw(img_gray);
-figure (1)
-imshow(img_b);
-SE=[1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]';
-number=imbothat(img_b,SE);   %obtem imagem oom apenas os numeros do barcode
-bar=imclose(img_b, SE);      %obtem imagem com apenas as barras do barcode 
-figure (2)
-imshow(number); 
-figure (3)
-imshow(bar)
+%% Image Reading
 
-%BW = edge(img_b, 'canny');
-%[H,T,R] = hough(BW);
-%x=T
+imgReaded = imread ('ean-13.jpg');
+imgGray = rgb2gray (imgReaded);
+img = im2double (imgGray);
+
+%%      Filtering
+
+%imgFilt = imgaussfilt (img);
+%figure, imshow (imgFilt);
+
+%%      Morphology
+
+se = strel ('line', 45, 0);
+imgMorph = imtophat (~(img), se);
+figure, imshow (imgMorph);
+
+%%      Rescale Image
 
 
+[x,y] = size (imgMorph);
+
+midx = round (x/2);
+
+ i = 1;
+ yinit = i+1;
+ while (imgMorph (midx, i) == 0)
+     i=i+1;
+     yinit = i;
+ end
+
+ i = y;
+ yend = i-1;
+ while (imgMorph (midx, i) == 0)
+      i=i-1;
+      yend = i;
+ end
+ 
+imgRescaled = imgMorph (midx:midx, yinit:yend);
+figure, imshow (imgRescaled);
+
+imgRescaled = imresize (imgRescaled, [1 95]);
+figure, imshow (imgRescaled);
+
+%%  BarCode Decodificaton
+
+% Check C1
+
+digit = 1;
+ C1 = imgRescaled (digit:digit+2);
+digit = digit + 3;
+ if (C1 ~= [1 0 1])
+     disp ('Error on C1');
+ end;
+ 
+ean13 = [0 0 0 0 0 0 0 0 0 0 0 0 0];
+
+
+ean13 (2) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (3) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (4) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (5) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (6) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (7) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+
+C2 = imgRescaled (digit:digit+4);
+digit = digit + 5;
+  if (C2 ~= [1 0 1 0 1])
+    disp ('Error on C2');   
+  end;
+
+ean13 (8) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (9) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (10) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (11) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (12) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
+ean13 (13) = EAN13digits(imgRescaled(digit: digit+6));
+digit = digit + 7;
